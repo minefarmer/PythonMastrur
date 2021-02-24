0ee7c71c8e2f67079097cea85d59d19112ca0fdf
minefarmer <pgoldfarm@yahoo.com>
2021-02-23 22:28:07 -0600

    good night

--- ClassFiles/DevelopeEnvironmentSetup/PythonCrashCourse/PrintFormatingSpecialCharacters.py
+++ ClassFiles/DevelopeEnvironmentSetup/PythonCrashCourse/PrintFormatingSpecialCharacters.py
@@ -1,10 +1,67 @@
-#                   Print formating and special charactors
+#                   Print formating
+#                   Special charactors  # 20
+
+
 
 # stock_price = "1100"
 # print("Today's price for Google stock is: " + stock_price)  # Today's price for Google stock is: 1100
 
 
-stock_price = "1100"
-print("Today's price for Google stock is: ()".format(stock_price))  # Today's price for Google stock is: 1100
+# stock_price = "1100"
+# print(f"Today's price for Google stock is: {stock_price}")  # Today's price for Google stock is: 1100
+
+
+# stock_price = "1100"
+# yesterdays_price = "1000"
+# print(f"Today's price for Google stock is: ()".format(stock_price))
+
+
+# today_price = "1100"
+# yesterdays_price = "1000"
+# print("Today's price:", today_price, "yesterdays_price", yesterdays_price)  # Today's price: 1100 yesterdays_price 1000
+# print("Today's price: {}, yesterdays_price: {}".format(today_price, yesterdays_price))  # Today's price: 1100 yesterdays_price 1000
+# print(f"Today's price: {today_price}, yesterday's price: {yesterdays_price}")  # Today's price: 1100, yesterday's price: 1000
+
+
+
+
+
+
+#                   Special charactors
+# \ = is an escape character, it escapes the special chararcter, following it is a string
+# \n = a newline within a string.
+# \t = adds a tab in it's place within the string.
+
+
+# print("My name is Carl Matson and \
+# not only do I know nothing, but I \
+# do nothing.")  # My name is Carl Matson and not only do I know nothing, but I do nothing.
+
+
+# print("""My name is Carl Matson and 
+#         not only do I know nothing, but I 
+# do nothing.""")  # My name is Carl Matson and 
+#                         # not only do I know nothing, but I 
+#                 # do nothing.
+
+
+# print("My name is Carl Matson and not only do I know nothing, but I do nothing.")   # My name is Carl Matson and not only do I know nothing, but I do nothing.
+
+
+# print("My name is Carl Matson\nand not only do I know nothing\nbut I do nothing.")  # My name is Carl Matson
+# and not only do I know nothing
+# but I do nothing.
+
+
+# print("My name is Carl Matson\n \tand not only do I know nothing\n \t\tbut I do nothing.")  # My name is Carl Matson
+                    # and not only do I know nothing
+                    #         but I do nothing.
+
+
+print("I'm using the \n special character to create new lines.")  # I'm using the 
+    #  special character to create new lines.
+
+
+print("I'm using the \\n special character to create new lines.")
 
 
